c5b8f73265ade6690a06e9fc44f5435e16297fce
Siva Swathi Vasagar <sivaswathi.vasagar@gmail.com>
2023-09-07 17:19:31 -0400

    Cleaning-up App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import "./App.css";
-
-import "./components/Header/header.css";
-
 import Nav from "./components/Nav";
 import Home from "./Pages/Home";
 import Blog from "./Pages/Blog";
@@ -9,32 +6,6 @@ import Header from "./components/Header/Header";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
-  // const [exercises, setExercise] = useState(null);
-
-  // const getExercise = async (searchTerm) => {
-  //   // const url = `https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?muscle=${searchTerm}`;
-  //   const url = `https://exerciseapi3.p.rapidapi.com/exercise/primary_muscle/${searchTerm}`;
-  //   const options = {
-  //     method: "GET",
-  //     headers: {
-  //       "X-RapidAPI-Key": "35bbfee14dmshdf2e85bd40a18a1p1f3574jsn3492cfda8fbd",
-  //       "X-RapidAPI-Host": "exerciseapi3.p.rapidapi.com",
-  //     },
-  //   };
-
-  //   // make fetch request and store response
-  //   try {
-  //     const response = await fetch(url, options);
-  //     const result = await response.json();
-  //     let a = result;
-  //     console.log(a);
-  //     setExercise(a);
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  //   //set the Movie state to the movie
-  // };
-
   return (
     <div className="App">
       <Header />
