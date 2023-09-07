735f83e2087e25baabc5ab6390f0ca545c23861e
Siva Swathi Vasagar <sivaswathi.vasagar@gmail.com>
2023-09-06 22:01:39 -0400

    Updated Form.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,32 @@
 import React from "react";
+import { useState } from "react";
 
-export default function Form() {
-  return <div className="Form"></div>;
+export default function Form(props) {
+  const [formData, setFormData] = useState({
+    searchterm: "",
+  });
+  const handleChange = (event) => {
+    //use the event object to detect key and value to update
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+  const handleSubmit = (event) => {
+    //prevent page from refreshing on form submission
+    event.preventDefault();
+    //pass the search term to moviesearch prop, which is apps getMovie function
+    props.exerciseSearch(formData.searchterm);
+  };
+
+  return (
+    <div className="Form">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="searchterm"
+          onChange={handleChange}
+          value={formData.searchterm}
+        />
+        <input type="submit" value="submit" />
+      </form>
+    </div>
+  );
 }
