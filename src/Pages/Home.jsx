c59f466ef94ae2f2433321fe52171cc31937b970
Siva Swathi Vasagar <sivaswathi.vasagar@gmail.com>
2023-09-07 17:20:49 -0400

    Modified Home.jsx file to adopt the new functionality

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,15 +3,16 @@ import Form from "../components/Form/Form";
 import GetExercise from "../components/GetExercise";
 import { useState } from "react";
 
-export default function Home(props) {
+export default function Home() {
   const [exercises, setExercise] = useState(null);
 
-  const getExercise = async (searchTerm) => {
+  const getExercise = async (selectedMuscleGroup) => {
     // const url = `https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?muscle=${searchTerm}`;
-    const url = `https://exerciseapi3.p.rapidapi.com/exercise/primary_muscle/${searchTerm}`;
+    const url = `https://exerciseapi3.p.rapidapi.com/exercise/primary_muscle/${selectedMuscleGroup}`;
     const options = {
       method: "GET",
       headers: {
+        "X-RapidAPI-Key": "35bbfee14dmshdf2e85bd40a18a1p1f3574jsn3492cfda8fbd",
         "X-RapidAPI-Host": "exerciseapi3.p.rapidapi.com",
       },
     };
@@ -20,18 +21,16 @@ export default function Home(props) {
     try {
       const response = await fetch(url, options);
       const result = await response.json();
-      let a = result;
-      console.log(a);
-      setExercise(a);
+      console.log(result);
+      setExercise(result);
     } catch (error) {
       console.error(error);
     }
-    //set the Movie state to the movie
   };
   return (
     <div className="Home">
       <h3>“Take care of your body. It’s the only place you have to live.”</h3>
-      <p>Welcome to my Fitness Planner!</p>
+      <h4>Welcome to my Fitness Planner!</h4>
       <p>
         Please choose the muscle group that you are planning to workout today
         from below options
