fbd6995aa952e28b9cf841ae24c7b9bc21f020aa
Siva Swathi Vasagar <sivaswathi.vasagar@gmail.com>
2023-09-12 22:11:23 -0400

    Updated function to loop over 2 properties

--- src/components/GetExercise.jsx
+++ src/components/GetExercise.jsx
@@ -16,37 +16,39 @@ export default function GetExercise({ exercises }) {
         advancedArray.push(Exercises);
       }
     });
-    for (let j = 0; j < 1; j++) {
+    for (let j = 0; j < 2; j++) {
       if (beginnerArray.length > 0) {
-        allExercisesArray.push(beginnerArray[0]);
+        allExercisesArray.push(beginnerArray[j]);
       }
     }
-    for (let j = 0; j < 1; j++) {
+    for (let j = 0; j < 2; j++) {
       if (intermediateArray.length > 0) {
-        allExercisesArray.push(intermediateArray[0]);
+        allExercisesArray.push(intermediateArray[j]);
       }
     }
-    for (let j = 0; j < 1; j++) {
+    for (let j = 0; j < 2; j++) {
       if (advancedArray.length > 0) {
-        allExercisesArray.push(advancedArray[0]);
+        allExercisesArray.push(advancedArray[j]);
       }
     }
 
     return (
       <div className="GetExercise">
         <h2>Here is the list</h2>
-        {allExercisesArray.map((allExercises, index) => (
-          <div className="AllExercise" key={index}>
-            <h3>Name: {allExercises.name}</h3>
-            <h4>Experience: {allExercises.experience}</h4>
-            <p>Equipment: {allExercises.equipment}</p>
-            <p>Force type: {allExercises.force_type}</p>
-            <p>Primary Muscles: {allExercises.primary_muscles}</p>
-            <p>Secondary Muscles: {allExercises.secondary_muscles}</p>
-            <p>Mechanics: {allExercises.mechanics}</p>
-            <p>Reps: 3 sets (12 to 15 repetitions/set)</p>
-          </div>
-        ))}
+        <div className="ExerciseList">
+          {allExercisesArray.map((allExercises, index) => (
+            <div className="AllExercise" key={index}>
+              <h3>Name: {allExercises.name}</h3>
+              <h4>Experience: {allExercises.experience}</h4>
+              <p>Equipment: {allExercises.equipment}</p>
+              <p>Force type: {allExercises.force_type}</p>
+              <p>Primary Muscles: {allExercises.primary_muscles}</p>
+              <p>Secondary Muscles: {allExercises.secondary_muscles}</p>
+              <p>Mechanics: {allExercises.mechanics}</p>
+              <p>Reps: 3 sets (12 to 15 repetitions/set)</p>
+            </div>
+          ))}
+        </div>
       </div>
     );
   };
